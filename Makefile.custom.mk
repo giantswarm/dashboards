2a3675d93d1b283155efde54a9a66ee8aa9dff88
Théo Brigitte <theo.brigitte@gmail.com>
2024-08-23 11:06:58 +0200

    nitpicking: align Makefile help text (#612)

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -6,24 +6,24 @@ SHELL:=/bin/bash -O globstar
 
 dashboards = helm/dashboards/dashboards/**/*.json helm/dashboards/charts/**/*.json
 
-install-tools: ## Install dependencies tools
+install-tools: ##        Install dependencies tools
 	./scripts/install-tools.sh
 
-update-alertmanager-mixin: ## Update Alertmanager mixin dashboards
+update-alertmanager-mixin: ##   Update Alertmanager mixin dashboards
 	./scripts/update-monitoring-mixin-dashboards.sh
 
-update-alloy-mixin: install-tools ## Update Alloy mixin dashboards
+update-alloy-mixin: install-tools ##        Update Alloy mixin dashboards
 	./alloy/update-alloy-mixin.sh
 
-update-kubernetes-mixin: ## Update Kubernetes mixin dashboards
+update-kubernetes-mixin: ##     Update Kubernetes mixin dashboards
 	./scripts/sync-kube-mixin.sh
 
-update-mimir-mixin: install-tools ## Update Mimir mixin dashboards
+update-mimir-mixin: install-tools ##        Update Mimir mixin dashboards
 	./mimir/update.sh
 
-update-mixin: update-alertmanager-mixin update-alloy-mixin update-kubernetes-mixin update-mimir-mixin ## Update all mixins dashboards
+update-mixin: update-alertmanager-mixin update-alloy-mixin update-kubernetes-mixin update-mimir-mixin ##        Update all mixins dashboards
 
-lint-dashboards: install-tools ## Run dashboard-linter for all dashboards in the helm/dashboards directory
+lint-dashboards: install-tools ##        Run dashboard-linter for all dashboards in the helm/dashboards directory
 		@for file in $(dashboards); do \
 			echo "------ Linting $$file"; \
 			dashboard-linter lint -c linter/config.yaml $$file; \
