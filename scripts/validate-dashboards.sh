5c618048d2944ee9fdab01a5613e19dc593754ea
Théo Brigitte <theo.brigitte@gmail.com>
2025-02-24 10:18:46 +0100

    Add dashboards validation script (#679)
    ---------
    
    Co-authored-by: Hervé Nicol <hervenicol@users.noreply.github.com>

---
+++ scripts/validate-dashboards.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# This script does some sanity checks on dashboards:
+# - Check for owner tag presence
+# - Match owners against list of team names retrieved from https://github.com/giantswarm/github
+# - Check for uid presence
+#
+# It won't change any files, and does not require any extra argument. Just run it from the root of the repository, then check its logs and return value.
+set -eu
+
+# List of files to exclude
+excludes=( "values.schema.json" "home.json" )
+excludes_string="$(IFS="|"; echo "${excludes[*]}")"
+
+# Find valid team names
+github_repo="https://github.com/giantswarm/github.git"
+github_repo_directory="$(mktemp -d -t validate-dashboards.XXXXXXXXXX)"
+trap 'rm -rf "$github_repo_directory"' EXIT
+git clone --depth 1 "$github_repo" "$github_repo_directory"
+cd "$github_repo_directory"
+mapfile teams < <(find repositories -name '*.yaml' -printf '%f\n' | cut -d. -f1)
+cd -
+echo "Found ${#teams[@]} teams:" ${teams[*]}
+
+# Check all dashboards
+failures=0
+log_length=19
+find helm/dashboards/ -name "*.json" | grep -vE "$excludes_string" | while read -r f; do
+    # Find owner tag
+    if owners=$(jq -r '.tags[]?|select(.|test("^owner:"))' "$f" 2>/dev/null); then
+      if [ -z "$owners" ]; then
+          printf "%-${log_length}s %s\n" "Owner tag missing" "$f"
+          failures=$((failures+1))
+          continue
+      fi
+      # Check all owner tags found
+      for owner in $owners; do
+        team="${owner#owner:}"
+        # Check if owner tag matches a team name
+        if [[ ! "${teams[*]}" =~ ${team} ]]; then
+            printf "%-${log_length}s %s\n" "Owner team invalid" "$f"
+            failures=$((failures+1))
+            continue
+        fi
+      done
+    else
+      printf "%-${log_length}s %s\n" "Error parsing" "$f"
+      failures=$((failures+1))
+      continue
+    fi
+
+    # Check uid
+    uid=$(jq -r '.uid|strings' "$f")
+    if [ -z "$uid" ]; then
+        printf "%-${log_length}s %s\n" "Missing uid" "$f"
+        failures=$((failures+1))
+        continue
+    fi
+
+    printf "%-${log_length}s %s\n" "OK" "$f"
+done
+
+test $failures -gt 0 && exit 1
