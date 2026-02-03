0ce95f87a397af55c477f412aa1305e780e6309b
Hervé Nicol <hervenicol@users.noreply.github.com>
2026-02-03 19:54:12 +0100

    Bump kube-mixins to 1.4.1 (#815)
    Co-authored-by: Herve Nicol <12008875+hervenicol@users.noreply.github.com>

--- scripts/sync-kube-mixin.sh
+++ scripts/sync-kube-mixin.sh
@@ -12,6 +12,19 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+unneeded_dashboards=(
+    k8s-resources-windows-cluster.json
+    k8s-resources-windows-namespace.json
+    k8s-resources-windows-pod.json
+    k8s-windows-cluster-rsrc-use.json
+    k8s-windows-node-rsrc-use.json
+    proxy.json
+    )
+
+public_dashboards=(
+    apiserver.json
+    )
+
 function tune_dashboard {
     dashboardFile="$1"
     # Extra tuning
@@ -19,14 +32,12 @@ function tune_dashboard {
     # Latest mixins use SLO instead of classic metrics in several places
     # but we dropped these SLO metrics
     sed -i 's/apiserver_request_slo_duration_seconds/apiserver_request_duration_seconds/g' "$dashboardFile"
+    sed -i 's/apiserver_request_sli_duration_seconds/apiserver_request_duration_seconds/g' "$dashboardFile"
 
     # Dashboard ownership.
     case $(basename "${dashboardFile}") in
         cluster-total.json|namespace-by-pod.json|namespace-by-workload.json|pod-total.json|workload-total.json)
             sed -i 's/"owner:team-.*"/"owner:team-cabbage"/g' "${dashboardFile}"
-            ;;
-        controller-manager.json|k8s-resources-cluster.json|k8s-resources-multicluster.json|k8s-resources-namespace.json|k8s-resources-node.json|k8s-resources-pod.json|k8s-resources-workloads-namespace.json|persistentvolumesusage.json|scheduler.json|statefulset.json)
-            sed -i 's/"owner:team-.*"/"owner:team-turtles"/g' "${dashboardFile}"
             ;;
     esac
 }
@@ -54,12 +65,23 @@ cd "${TMPDIR}"/mixins
 MIXIN_VER=$(git rev-parse HEAD)
 cd - > /dev/null
 
+# Remove unneeded dashboards
+for dashboard in "${unneeded_dashboards[@]}"; do
+    rm -f "${TMPDIR}"/mixins/files/dashboards/"${dashboard}"
+done
+
+# Tune dashboards as needed
 while read -r file; do
     tune_dashboard "$file"
 done < <(find "$TMPDIR" -type f -name \*.json)
 
-# copy over the dashboards to the helm chart
-cp -a "${TMPDIR}"/mixins/files/dashboards/* helm/dashboards/dashboards/mixin/
+# Move public dashboards to the publec dashboards folder
+for dashboard in "${public_dashboards[@]}"; do
+    mv "${TMPDIR}"/mixins/files/dashboards/"${dashboard}" helm/dashboards/charts/public_dashboards/dashboards/shared/public/
+done
+
+# Move remaining (private) dashboards to the helm chart
+mv "${TMPDIR}"/mixins/files/dashboards/* helm/dashboards/dashboards/mixin/
 
 echo -e "\nSynced mixin repo at commit: ${MIXIN_VER}\n"
 
